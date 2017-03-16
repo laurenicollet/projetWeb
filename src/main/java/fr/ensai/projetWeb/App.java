e7de45afc30eb2f33eb9c8bfc975c145e41604f0
laure <laure.nicollet@eleve.ensai.fr>
2017-03-16 10:49:47 +0100

    fin de la partie java

--- src/main/java/fr/ensai/projetWeb/App.java
+++ src/main/java/fr/ensai/projetWeb/App.java
@@ -10,60 +10,59 @@ import org.renjin.script.*;
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		//main method to launch everything
-
-		//ici, le DSL devra remplacer "Paris", "Rennes" et "naissances" 
-		//par ce que l'utilisateur aura écrit en utilisantnotre langage
-		Data d = new Data(new Ville("rennes"), new Ville("rennes"), new Theme("election"));
-
+		
+		//Getting the datasets available for the choosen cities and theme
+		Data d = new Data(new City("paris"), new City("toulouse"), new Theme("naissance"));
 		d.getData();
-
-		//choix par l'utilisateur des datasets pour chaque ville
+		
+		//Choice by the user of the datasets he wants to use in each city
 		chooseDatasets(d);
-		System.out.println("Vous avez choisi d'utiliser "+d.v1.datasets.get(d.v1.choosenDataset)+" pour "+d.v1.name
-				+" et "+d.v2.datasets.get(d.v2.choosenDataset)+" pour "+d.v2.name);
-
-		//choix par l'utilisateur des colonnes pour chaque dataset
+		System.out.println("You will use '"+d.c1.datasets.get(d.c1.choosenDataset)+"' for "+d.c1.name
+				+"\n and '"+d.c2.datasets.get(d.c2.choosenDataset)+"' for "+d.c2.name);
+		
+		//Choice by the user of the columns he wants to use in each dataset
 		chooseColumns(d);
-		System.out.println("Vous avez choisi d'utiliser la colonne '"+d.v1.fields.get(d.v1.choosenDataset).get(d.v1.choosenColumn)+
-				"' des données '"+d.v1.datasets.get(d.v1.choosenDataset)
-				+"' de la ville de "+d.v1.name
-				+"\n et la colonne '"+d.v2.fields.get(d.v2.choosenDataset).get(d.v2.choosenColumn)
-				+"' des données '"+d.v2.datasets.get(d.v2.choosenDataset)
-				+"' de la ville de "+d.v2.name);
-
-		/*
-		 * TODO
-		 * 
-		 * calculer la correlation / tracer le graphique (utilisation de Renjin ou JSAT ?) sur les objets
-		 * d.v1.getDataset("nom du dataset choisi dans la ville 1").getColumn("nom de la 1e colonne choisi")
-		 * d.v1.getDataset("nom du dataset choisi dans la ville 1").getColumn("nom de la 2e colonne choisi")
-		 * d.v2.getDataset("nom du dataset choisi dans la ville 1").getColumn("nom de la 3e colonne choisi")
-		 * d.v2.getDataset("nom du dataset choisi dans la ville 1").getColumn("nom de la 4e colonne choisi")
-		 */
-
-
-		//exemple pour faire de R en java :
+		System.out.println("You will use the column '"+d.c1.fields.get(d.c1.choosenDataset).get(d.c1.choosenColumn)+
+				"' of the data '"+d.c1.datasets.get(d.c1.choosenDataset)
+				+"' for the city of "+d.c1.name
+				+"\n and the column '"+d.c2.fields.get(d.c2.choosenDataset).get(d.c2.choosenColumn)
+				+"' of the data '"+d.c2.datasets.get(d.c2.choosenDataset)
+				+"' for the city of "+d.c2.name);
+		
+		//Getting data for the choosen columns and datasets
+		d.getDatasets(d.c1);
+		d.getDatasets(d.c2);
+		
+		System.out.println("-----------------------------------------------------------------------------------");
+		System.out.println("Your choice have been taken into account, the computation will start now...");
+		System.out.println("-----------------------------------------------------------------------------------");
+		//computation of the correlation and plot of the result with R code
 		RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
-		ScriptEngine engine = factory.getScriptEngine();
-		try {
-			engine.eval("library(jsonlite)");
-
-			engine.eval("df <- data.frame(x=1:10, y=(1:10)+rnorm(n=10))");
-			engine.eval("print(df)");
-			engine.eval("print(lm(y ~ x, df))");
-
-			engine.put("toto", d.v1.datasets);
-			engine.eval("print(typeof(toto))");
-
-
+	    ScriptEngine engine = factory.getScriptEngine();
+	    try {
+	    	engine.eval("library(jsonlite)");
+			engine.eval("print('The correlation and the plot should be displayed here but the R code does not work yet...')");
+			engine.eval("print('Please come back in a few years ! ;) ')");
+	    	
+			/*
+	    	 * TODO
+	    	 * 
+	    	 * The json d.v1.data et d.v2.data must be parsed with the library jsonlite
+	    	 * Then the data must be formated to have the same length (for exemple, one can easily
+	    	 * truncate the longest vector at the size of the shortest vector)
+	    	 * The correlation can be calculated with the R function 'cor' :
+	    	 * engine.eval("cor(data.frame(v1=d.v1.data, v2=d.v2.data), method = 'pearson')")
+	    	 * It would also be great to display the plot but the plot function is not implemented
+	    	 * in renjin yet.
+	    	 * 
+	    	 */
+	    	
 		} catch (ScriptException e) {
 			e.printStackTrace();
 		}
-
+	    
 	}
-	
-	
+
 	
 	public static void chooseDatasets(Data d){	
 		int i=0;
@@ -71,47 +70,47 @@ public class App {
 		HashMap<Integer, String> id2 = new HashMap<Integer, String>();
 		Scanner sc = new Scanner(System.in);
 
-		//ajouter une exception s'il n'y a pas de datasets pour la ville et le theme choisi
+		//TODO : add an exception when there's no datasets available for the sity and the theme
 
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Voici les données disponibles pour la première ville ("+d.v1.name+") : ");
+		System.out.println("These datasets are available for the first city ("+d.c1.name+") : ");
 		System.out.println("---------------------------------------------------------");
-		for(String s : d.v1.datasets.keySet()){
-			System.out.println(i+" : "+d.v1.datasets.get(s));
+		for(String s : d.c1.datasets.keySet()){
+			System.out.println(i+" : "+d.c1.datasets.get(s));
 			id1.put(i, s);
 			i++;
 		}
 
 		i=0;
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Voici les données disponibles pour la seconde ville ("+d.v2.name+") : ");
+		System.out.println("These datasets are available for the second city  ("+d.c2.name+") : ");
 		System.out.println("---------------------------------------------------------");
-		for(String s : d.v2.datasets.keySet()){
-			System.out.println(i+" : "+d.v2.datasets.get(s));
+		for(String s : d.c2.datasets.keySet()){
+			System.out.println(i+" : "+d.c2.datasets.get(s));
 			id2.put(i, s);
 			i++;
 		}
 		
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Veuillez saisir le nombre correspondant à votre choix pour la première ville ("+d.v1.name+") : ");
+		System.out.println("Please enter the number of your choice for the first city ("+d.c1.name+") : ");
 		System.out.println("---------------------------------------------------------");
 		int str = sc.nextInt();
 		while(str>=id1.size() || str<0){
-			System.out.println("Veuillez choisir un nombre entre 0 et "+(id1.size()-1));
+			System.out.println("You must choose a number between 0 and "+(id1.size()-1));
 			str = sc.nextInt();
 		}
-		d.v1.choosenDataset = id1.get(str);
+		d.c1.choosenDataset = id1.get(str);
 		
 		
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Veuillez saisir le nombre correspondant à votre choix pour la seconde ville ("+d.v1.name+") : ");
+		System.out.println("Please enter the number of your choice for the second city ("+d.c2.name+") : ");
 		System.out.println("---------------------------------------------------------");
 		str = sc.nextInt();
 		while(str>=id2.size() || str<0){
-			System.out.println("Veuillez choisir un nombre entre 0 et "+(id2.size()-1));
+			System.out.println("You must choose a number between 0 and "+(id2.size()-1));
 			str = sc.nextInt();
 		}
-		d.v2.choosenDataset = id2.get(str);
+		d.c2.choosenDataset = id2.get(str);
 	}
 	
 	
@@ -123,42 +122,41 @@ public class App {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Voici les colonnes disponibles pour les premières données ("+d.v1.datasets.get(d.v1.choosenDataset)+") : ");
+		System.out.println("These columns are available for the first dataset ("+d.c1.datasets.get(d.c1.choosenDataset)+") : ");
 		System.out.println("---------------------------------------------------------");
-		for(String s : d.v1.fields.get(d.v1.choosenDataset).keySet()){
-			System.out.println(i+" : "+d.v1.fields.get(d.v1.choosenDataset).get(s));
+		for(String s : d.c1.fields.get(d.c1.choosenDataset).keySet()){
+			System.out.println(i+" : "+d.c1.fields.get(d.c1.choosenDataset).get(s));
 			id1.put(i, s);
 			i++;
 		}
 		i=0;
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Voici les colonnes disponibles pour les secondes données ("+d.v2.datasets.get(d.v2.choosenDataset)+") : ");
+		System.out.println("These columns are available for the second dataset  ("+d.c2.datasets.get(d.c2.choosenDataset)+") : ");
 		System.out.println("---------------------------------------------------------");
-		for(String s : d.v2.fields.get(d.v2.choosenDataset).keySet()){
-			System.out.println(i+" : "+d.v2.fields.get(d.v2.choosenDataset).get(s));
+		for(String s : d.c2.fields.get(d.c2.choosenDataset).keySet()){
+			System.out.println(i+" : "+d.c2.fields.get(d.c2.choosenDataset).get(s));
 			id2.put(i, s);
 			i++;
 		}
 		
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Veuillez saisir le nombre correspondant à votre choix pour les premières données ("+d.v1.datasets.get(d.v1.choosenDataset)+") : ");
+		System.out.println("Please enter the number of your choice for the first dataset ("+d.c1.datasets.get(d.c1.choosenDataset)+") : ");
 		System.out.println("---------------------------------------------------------");
 		int str = sc.nextInt();
 		while(str>=id1.size() || str<0){
-			System.out.println("Veuillez choisir un nombre entre 0 et "+(id1.size()-1));
+			System.out.println("You must choose a number between 0 and "+(id1.size()-1));
 			str = sc.nextInt();
 		}
-		d.v1.choosenColumn = id1.get(str);
+		d.c1.choosenColumn = id1.get(str);
 		
 		System.out.println("---------------------------------------------------------");
-		System.out.println("Veuillez saisir le nombre correspondant à votre choix pour les secondes données ("+d.v2.datasets.get(d.v2.choosenDataset)+") : ");
+		System.out.println("Please enter the number of your choice for the second dataset ("+d.c2.datasets.get(d.c2.choosenDataset)+") : ");
 		System.out.println("---------------------------------------------------------");
 		str = sc.nextInt();
 		while(str>=id2.size() || str<0){
-			System.out.println("Veuillez choisir un nombre entre 0 et "+(id2.size()-1));
+			System.out.println("You must choose a number between 0 and "+(id2.size()-1));
 			str = sc.nextInt();
 		}
-		d.v2.choosenColumn = id2.get(str);
+		d.c2.choosenColumn = id2.get(str);
 	}
-
 }
